67169956a6e01dcd88d31b1490d95d01ddbbffe8
Marcus Pauli <info@mpauli.de>
2020-03-29 22:17:25 +0000

    Update mhn.py
    port=8080 to avoid comflict with Splunk

--- server/mhn.py
+++ server/mhn.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
+##mp port=8080 to avoid comflict with Splunk
 
 if __name__ == '__main__':
     from mhn import mhn
-    mhn.run(debug=False, host='0.0.0.0', port=8000)
+    mhn.run(debug=False, host='0.0.0.0', port=8080)
